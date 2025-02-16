17f0e3420ae2219a64c6b2c22ef3b16c9ecd9b71
Fellyph Cintra <fellyph.cintra@gmail.com>
2025-02-16 19:13:59 +0000

    Implement structured output for river data using Gemini API schema

--- final-demo-2-structured-output.ts
+++ final-demo-2-structured-output.ts
@@ -1,5 +1,9 @@
 // PROMP CONFIGURATION
-import { GoogleGenerativeAI, GenerativeModel } from "@google/generative-ai";
+import {
+    GoogleGenerativeAI,
+    GenerativeModel,
+    SchemaType,
+} from "@google/generative-ai";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
 
@@ -12,17 +16,40 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || "");
 // 2. Generate text
 async function generateText(): Promise<void> {
     try {
+        const schema = {
+            description: "List of rivers",
+            type: SchemaType.ARRAY,
+            items: {
+                type: SchemaType.OBJECT,
+                properties: {
+                    riverName: {
+                        type: SchemaType.STRING,
+                        description: "Name of the river",
+                        nullable: false,
+                    },
+                    riverLength: {
+                        type: SchemaType.NUMBER,
+                        description: "Lenght of the river in KM",
+                        nullable: false,
+                    },
+                },
+                required: ["riverName", "riverLength"],
+            },
+        };
+
         const model: GenerativeModel = genAI.getGenerativeModel({
             model: "gemini-2.0-flash-001",
+            generationConfig: {
+                responseMimeType: "application/json",
+                responseSchema: schema,
+            },
         });
 
         /* const prompt = `List 5 main river in Portugal in JSON code using this JSON schema:
                             River = {'riverName': string, 'riverLength': number}
                             Return: Array<River>`; */
 
-        const prompt = `List 5 main river in Portugal in JSON code using this JSON schema:
-                            River = {'riverName': string, 'riverLength': number}
-                            Return: Array<River>`;
+        const prompt = `List 5 main river in Portugal`;
 
         const result = await model.generateContent(prompt);
         const response = await result.response;
