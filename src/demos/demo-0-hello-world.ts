0c207d015589485aea4c1eca0ed97daf6606ca03
Fellyph Cintra <fellyph.cintra@gmail.com>
2025-03-11 13:41:32 +0100

    Update hello world demo to use Gemini model with chat session

--- src/demos/demo-0-hello-world.ts
+++ src/demos/demo-0-hello-world.ts
@@ -9,13 +9,31 @@ dotenv.config();
 // 1. configure the API key
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
 
-// 2. Generate text
-async function generateText(): Promise<void> {
-    try {
-        console.log('Hello, world!');
-    } catch (error) {
-        console.error('Error:', error);
-    }
+const model = genAI.getGenerativeModel({
+    model: 'gemini-2.0-pro-exp-02-05',
+});
+
+const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: 'text/plain',
+};
+
+async function run() {
+    const chatSession = model.startChat({
+        generationConfig,
+        history: [
+            {
+                role: 'user',
+                parts: [{ text: 'when is the next befica match' }],
+            },
+        ],
+    });
+
+    const result = await chatSession.sendMessage('INSERT_INPUT_HERE');
+    console.log(result.response.text());
 }
 
-generateText();
+run();
