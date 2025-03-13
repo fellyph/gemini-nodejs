0c44d3c265e6cfd9a77291462228ec3391610ab7
Fellyph Cintra <fellyph.cintra@gmail.com>
2025-03-13 17:59:36 +0100

    adding new js sdk

--- src/demos/demo-0-hello-world.ts
+++ src/demos/demo-0-hello-world.ts
@@ -1,13 +1,12 @@
 // DEMO 0 - Hello World
-import { GoogleGenerativeAI, GenerativeModel } from '@google/generative-ai';
+import { GoogleGenAI } from '@google/genai';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 
 dotenv.config();
 (global as any).fetch = fetch;
 
-// 1. configure the API key
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
+const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
 
 // 2. Generate text
 async function generateText(): Promise<void> {
